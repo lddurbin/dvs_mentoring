2090259364936f1591090167a67784255b84ddcf
lddurbin <l.d.durbin@gmail.com>
2022-06-26 10:39:28 +1200

    Basic line chart with improvements suggested by mentees

--- lines/NZ_vaccinations_original.R
+++ lines/NZ_vaccinations_original.R
@@ -10,3 +10,19 @@ vaccinations <- read_excel(
   tidyr::pivot_longer(2:3, names_to = "dose") |> 
   mutate(date = as.Date(date))
 
+ggplot(vaccinations, aes(x = date, y = value, colour = dose)) +
+  geom_line(size = 1.5) +
+  scale_x_date(limits = range, breaks = "1 month", labels = scales::label_date("%B")) +
+  scale_y_continuous(labels = scales::label_comma()) +
+  scale_color_manual(labels = c("1st dose", "2nd dose"), values = c("blue", "red")) +
+  labs(
+    y = "Number of doses administered",
+    title = "Number of doses of the COVID-19 vaccine administered by day in 2021",
+    colour = "Vaccine dosages"
+  ) +
+  theme(
+    plot.title.position = "plot",
+    title = element_text(size = 18)
+  )
+
+# daily average as well as 1st and 2nd doses
