55254eb3d70b4c00fb2e810c434441d0b087c339
lddurbin <l.d.durbin@gmail.com>
2022-07-03 20:23:44 +1200

    Replicated the R code in Python

--- lines/NZ_vaccinations_python.py
+++ lines/NZ_vaccinations_python.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from skimpy import clean_columns
+import plotnine as p9
 
 for file in os.listdir("Code/dvs_mentoring/lines"):
     if file.endswith(".xlsx"):
@@ -11,3 +12,8 @@ df = pd.read_excel (excel, sheet_name = 'Date')
 clean_df = clean_columns(df)
 
 vaccinations = pd.melt(clean_df, id_vars='date', value_vars=['first_dose_administered', 'second_dose_administered'], var_name='dose')
+
+my_plot = (p9.ggplot(vaccinations, p9.aes('date', 'value', color='dose'))
+    + p9.geom_line(size=1.5))
+
+my_plot.save("Code/dvs_mentoring/lines/lines.png", dpi=300)
