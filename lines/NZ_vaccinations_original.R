59ade7139bacd73ed6fdbdc91403b69bc2cce324
lddurbin <l.d.durbin@gmail.com>
2022-07-04 10:11:54 +1200

    Add the legend into the title and replace the standalone legend, replace the line chart with geom_smooth, remove the grey background and all gridlines.

--- lines/NZ_vaccinations_original.R
+++ lines/NZ_vaccinations_original.R
@@ -1,6 +1,7 @@
 library(dplyr) # A Grammar of Data Manipulation
 library(ggplot2) # Create Elegant Data Visualisations Using the Grammar of Graphics
 library(readxl) # Read Excel Files
+library(ggtext)
 
 vaccinations <- read_excel(
   path = fs::dir_ls(here::here("lines"), glob = "*.xlsx"),
@@ -11,18 +12,23 @@ vaccinations <- read_excel(
   mutate(date = as.Date(date))
 
 ggplot(vaccinations, aes(x = date, y = value, colour = dose)) +
-  geom_line(size = 1.5) +
+  geom_smooth(se = FALSE, size = 1.5) +
   scale_x_date(limits = range, breaks = "1 month", labels = scales::label_date("%B")) +
   scale_y_continuous(labels = scales::label_comma()) +
   scale_color_manual(labels = c("1st dose", "2nd dose"), values = c("blue", "red")) +
   labs(
     y = "Number of doses administered",
-    title = "Number of doses of the COVID-19 vaccine administered by day in 2021",
+    title = "Number of <span style='color:blue'><strong>first doses</strong></span> and number of <span style='color:red'><strong>second doses<strong></span> of the COVID-19 vaccine administered by day in 2021",
     colour = "Vaccine dosages"
   ) +
+  theme_minimal() +
   theme(
     plot.title.position = "plot",
-    title = element_text(size = 18)
+    title = element_text(size = 18),
+    legend.position = "none",
+    plot.title = element_markdown(lineheight = 1.1),
+    axis.title = element_blank(),
+    panel.grid = element_blank()
   )
 
 # daily average as well as 1st and 2nd doses
